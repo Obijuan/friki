6a7b179055602a9f367632420545f89e0774b939
obijuan <juan@iearobotics.com>
2015-03-20 16:04:00 +0100

    Initial drafts of cube, union and part classes

--- pyooml.py
+++ pyooml.py
@@ -4,23 +4,77 @@
 import FreeCAD
 import Part
 
-class cube(object):
+class part(object):
+	"""Generic objects"""
+	
+	def __init__(self):
+		print("new part")
+	
+	# overload +
+	def __add__(self, other):
+		print("Parts sum!!! {} + {}".format(self.obj.Label, other.obj.Label))
+
+		#-- Return the union of the two objects
+		return union(self, other)
+
+class union(part):
+	"""Union of objects"""
+	
+	def __init__(self, part1, part2):
+		#-- Call the parent class constructor first
+		super(union, self).__init__()
+		
+		#-- Create the Freecad Object
+		doc = FreeCAD.activeDocument()
+		self.obj = doc.addObject("Part::MultiFuse","Union")
+
+		#-- Do the union!
+		self.obj.Shapes = [part1.obj, part2.obj]
+
+		doc.recompute()
+		print("Union!")
+		
+
+class cube(part):
 	"""Primitive Object: a cube"""
 	
-	def __init__(self, lx = 10):
+	def __init__(self, lx = 10, ly = 10, lz = 10):
+		"""Create a primitive cube:
+			lx: length in x axis
+			ly: length in y axis
+			lz: length in z axis"""
+		
+		#-- Call the parent class constructor first
+		super(cube, self).__init__()
+		
+		#-- Create the Freecad Object
 		self.obj = FreeCAD.ActiveDocument.addObject("Part::FeaturePython","Cube")
+
 		#--Add property
+		
+		#-- Configure the object for working ok in the Freecad environment	
 		self.obj.Proxy = self
 		self.obj.ViewObject.Proxy = self
+		
+		#-- Show the object!
 		FreeCAD.ActiveDocument.recompute()
 		print ("Cube Init!")
-
+	
+	def __str__(self):
+		str_id = "cube({}, {}, {}). Label: {}".format(10, 10, 10, self.obj.Label)
+		return str_id	
+	
 	def translate(self, x, y, z):
 		"""Translate the object"""
 
 		#-- Get the translation vector
 		v = FreeCAD.Vector(x, y, z)
-		self.obj.Placement.Base = v
+		
+		#-- Apply the translation (relative to the current position)
+		self.obj.Placement.Base += v
+
+		print("hola")
+		return self
 	
 	def execute(self, obj):
 		"""Build the object"""
