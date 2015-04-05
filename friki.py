5e08b6a3309f4fce831e0b3382661b47d5de8f15
obijuan-test <juan@iearobotics.com>
2015-04-05 09:36:52 +0200

    Barrientos' book Example 3.6. Pag. 89

--- friki.py
+++ friki.py
@@ -10,7 +10,7 @@ import FreeCAD
 import pyooml
 import HMatrix
 
-from pyooml import frame, svector
+from pyooml import frame, svector, cube, cylinder
 from FreeCAD import Vector
 
 #-- Exercise. Barrientos book. 79. Example. 3.1
@@ -127,13 +127,42 @@ def robot_model():
 	arm = cube(10, 80, 30).translate(-25, -15, 54)
 	forehand = cube(10, 60, 15, center = True).rotx(-45).translate(-10., 70, 50)
 
+def barrientos_ex3_6_pag_89():
+	#Initial frame
+	f0 = frame()
+	
+	#-- Define the transformations
+	M1 = HMatrix.Rotx(-90)
+	p = Vector(5, 5, 10)
+	M2 = HMatrix.Translation(p)
+	M3 = HMatrix.Rotz(90)
+	
+	#------- Apply the transformations
+	#-- 1st transformation
+	f1 = frame()
+	f1.T = M1
+	
+	#-- 2nd transformation
+	#-- As the transformation is relative to the global xyz frame, 
+	#-- the matrices should be multiplied en reverse order
+	f2 = frame()
+	f2.T = M2 * M1
+	
+	#-- 3rd transformation
+	f3 = frame()
+	f3.T = M3 * M2 * M1
+	
+	print("Result:")
+	print(f3.T)
+
 if __name__ == "__main__":
 	#barrientos_pag79_ex3_1()
 	#barrientos_ex3_2_pag_80()
 	#barrientos_ex3_3_pag_81()
 	#barrientos_ex3_4_pag_84()
 	#barrientos_ex3_5_pag_84()
-	robot_model()
+	#robot_model()
+	barrientos_ex3_6_pag_89()
 
 
 	
